c69a64afe0309e7de603d385e2e8d2b22ccb7708
Vladislava Scherbo <vladislava.volokhovich@gmail.com>
2025-10-20 13:53:13 +0300

    Initial Commit

---
+++ project8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project8
+//
+//  Created by Vladislava Scherbo on 20.10.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
